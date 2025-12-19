2ba55362987d07152412fce59c490873a6c1722d
Hemachandar <hmac.devo@gmail.com>
2025-12-19 17:38:55 +0530

    esm, cjs

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from "rolldown";
 
 export default defineConfig({
   input: "src/index.ts",
-  output: {
-    file: "dist/index.js",
-    format: "esm",
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: "dist/esm/index.js",
+      format: "esm",
+    },
+    {
+      file: "dist/cjs/index.cjs",
+      format: "cjs",
+    },
+  ],
   tsconfig: "tsconfig.json",
   platform: "browser",
-  external: [
-    "react",
-    "react-dom",
-    "react/jsx-runtime",
-  ],
+  external: ["react", "react-dom", "react/jsx-runtime"],
 });
